29bd2c6c8b8fee942fbdfd90f8575a5a6b35ec47
Bethel D <88989326+betheld@users.noreply.github.com>
2021-10-25 18:59:59 -0400

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://betheld:HntIVmurguS2COzY@cluster0.ikrip.mongodb.net/roulette?retryWrites=true&w=majority" ,
+    'dbName': 'SavageAuth'
+};

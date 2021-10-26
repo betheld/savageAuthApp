a87b6cc0578ea72a06efcf8e9c6bfed77de986fa
Bethel D <88989326+betheld@users.noreply.github.com>
2021-10-26 10:38:51 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
-// config/database.js
 module.exports = {
 
-    'url' : "mongodb+srv://betheld:HntIVmurguS2COzY@cluster0.ikrip.mongodb.net/roulette?retryWrites=true&w=majority" ,
-    'dbName': 'SavageAuth'
+    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'demo'
 };
